c5bedfb28ba572c62c0cb3d09a7410578d04d2c4
Jason <jason.blunck@gmail.com>
2022-06-09 10:30:43 -0500

    Made cipher program into a function

--- week1/day3/caesarCipher.py
+++ week1/day3/caesarCipher.py
@@ -1,9 +1,14 @@
 cipher = "lbh zhfg hayrnea jung lbh unir yrnearq"
 
-for i in cipher:
-    if i == " ":
-        print(" ", end="")
-    elif ord(i)< ord("a")+13:
-        print(chr(ord(i)+13), end="")
-    else:
-        print(chr(ord(i)-13), end="")
+def cipherSolver(cipher):
+    for i in cipher:
+        if i == " ":
+            print(" ", end="")
+        elif ord(i)< ord("a")+13:
+            print(chr(ord(i)+13), end="")
+        else:
+            print(chr(ord(i)-13), end="")
+
+    return ""
+
+print(cipherSolver(cipher))
