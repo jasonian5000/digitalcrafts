fdb2c4c7903c54cfbc6ce39b762a94024442b73d
Jason <jason.blunck@gmail.com>
2022-06-09 10:25:15 -0500

    solved Caesar Cipher problem

--- week1/day3/caesarCipher.py
+++ week1/day3/caesarCipher.py
@@ -3,7 +3,7 @@ cipher = "lbh zhfg hayrnea jung lbh unir yrnearq"
 for i in cipher:
     if i == " ":
         print(" ", end="")
-    if ord(i)< ord("a")+13:
+    elif ord(i)< ord("a")+13:
         print(chr(ord(i)+13), end="")
     else:
         print(chr(ord(i)-13), end="")
