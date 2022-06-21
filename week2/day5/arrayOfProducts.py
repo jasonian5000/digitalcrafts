c656837cc2cd8ebaff47f38af2612d75d149ec2f
Jason <jason.blunck@gmail.com>
2022-06-21 08:56:22 -0500

    weeke 2 day 5 practice

--- week2/day5/arrayOfProducts.py
+++ week2/day5/arrayOfProducts.py
@@ -1,13 +1,18 @@
 array = [5, 1, 4, 2]
 
 def products(array):
-    multiplied=[]
-    bigTotal=1
-    for i in array:
-        bigTotal *= i
-    for j in array:
-        multiplied.append(bigTotal//j)
-    print(multiplied)
+    arrayNest = [array for i in range(len(array))]
+    print(arrayNest)
+    # for j in range(len(arrayNest)):
+    #     arrayNest[j][j]=1
+    # print(arrayNest)
+    totalList=[]
+    total=1
+    for k in range(len(arrayNest)):
+        total*=arrayNest[k][k]
+        totalList.append(total)
+    print(arrayNest)
 
+        
 products(array)
         
