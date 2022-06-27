882161fd7c963768c1e325e0224f56fe6ac45f6d
Jason <jason.blunck@gmail.com>
2022-06-26 21:38:58 -0500

    more flexbox practice

---
+++ week3/day4/javascript-preview/practice.js
@@ -0,0 +1,29 @@
+// comment
+//variable
+// let, var const
+// let and var can be reassigned their values to different datatypes
+//const can only be the value it is assigned and nothing else
+// it can have stuff added to it
+
+let rahmin = "gamer";
+var carlos = "meme lord";
+const jason = "problems";
+
+const restaurants = ["uchi", "bnb", "gyu-kaku"]
+
+for (let index = 0; index < restaurants.length; index++) {
+    const restaurant = restaurants[index];
+    console.log(restaurant);
+}
+
+for(let restaurant of restaurants){
+    console.log(restaurant)
+}
+
+function TwoSum(){
+    let name = "rayleigh";
+    name = "not the ta anymore";
+    return "bye felicia";
+}
+console.log(TwoSum());
+
