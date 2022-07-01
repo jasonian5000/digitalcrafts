68042c08b4b3151a71ee0e5c81a6e1d7fd60ac02
Jason <jason.blunck@gmail.com>
2022-07-01 16:38:25 -0500

    Corrected blackjack game logic to handle ace value change and multiple aces in the same hand

--- week4/day4/blackjack-exercise/scripts/main.js
+++ week4/day4/blackjack-exercise/scripts/main.js
@@ -1,14 +1,13 @@
-// dom manipulation
 const dealerHand = document.getElementById("dealer-hand");
 const playerHand = document.getElementById("player-hand");
 const hitBtn = document.getElementById("hit-button");
 const dealBtn = document.getElementById("deal-button");
 const standBtn = document.getElementById("stand-button");
-const resetBtn = document.getElementById("reset")
-const message = document.getElementById("message-area")
-const p = document.createElement("p")
-
-// starting variables
+const resetBtn = document.getElementById("reset");
+const message = document.getElementById("message-area");
+const p = document.createElement("p");
+let aceCounter = 0;
+let aceCompare = 0;
 let dealerScore = 0;
 let playerScore = 0;
 let dealerCards = [];
@@ -16,8 +15,6 @@ let playerCards = [];
 let deck = [];
 const suits = ["hearts", "spades", "clubs", "diamonds"];
 const ranks = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
-
-// fuctions
 const makeDeck = () => {
   for (let suit of suits) {
     for (const rank of ranks) {
@@ -32,7 +29,6 @@ const makeDeck = () => {
     }
   }
 };
-
 const addCard = (hand, deck, cards, score) => {
   let img = new Image();
   img.src = deck[0].imageSrc;
@@ -42,23 +38,16 @@ const addCard = (hand, deck, cards, score) => {
   deck.splice(0, 1);
   return score;
 };
-
 const deal = () => {
-  // location.reload()
   makeDeck();
-  console.log(deck)
   dealerScore = addCard(dealerHand, deck, dealerCards, dealerScore);
   playerScore = addCard(playerHand, deck, playerCards, playerScore);
   dealerScore = addCard(dealerHand, deck, dealerCards, dealerScore);
   playerScore = addCard(playerHand, deck, playerCards, playerScore);
-    console.log(dealerCards);
-    console.log(playerCards);
-    console.log(deck)
   dealBtn.disabled = true;
   hitBtn.disabled = false;
   standBtn.disabled = false;
   if (playerScore === 21 && dealerScore === 21) {
-    console.log("What?! It's a tie.");
     p.innerText = "What?! It's a tie";
     message.appendChild(p);
   }
@@ -69,31 +58,27 @@ const deal = () => {
     lose();
   }
 };
-
 const hit = () => {
   if (playerCards.length < 2) {
-    console.log("You have to deal first!");
     p.innerText = "You have to deal first!";
     message.appendChild(p);
   } else {
     playerScore = addCard(playerHand, deck, playerCards, playerScore);
   }
   isAce = aceCheck();
-  console.log(isAce);
-  if (playerScore > 21 && isAce === false) {
+  if (playerScore > 21 && isAce === aceCompare) {
     lose();
   }
-  if (playerScore > 21 && isAce === true) {
+  if (playerScore > 21 && isAce > aceCompare) {
     playerScore -= 10;
+    aceCompare += 1;
   }
   if (playerScore === 21) {
     win();
   }
 };
-
 const stand = () => {
   if (playerCards.length < 2) {
-    console.log("You have to deal first!");
     p.innerText = "You have to deal first!";
     message.appendChild(p);
   } else {
@@ -108,42 +93,29 @@ const stand = () => {
     lose();
   }
 };
-
 const win = () => {
-  console.log("You win!");
   p.innerText = "You win!";
   message.appendChild(p);
   hitBtn.disabled = true;
   standBtn.disabled = true;
 };
-
 const lose = () => {
-  console.log("You lose :(");
-  p.innerText = "You lose!"
-  message.appendChild(p)
+  p.innerText = "You lose!";
+  message.appendChild(p);
   hitBtn.disabled = true;
   standBtn.disabled = true;
 };
-
 const aceCheck = () => {
-  console.log(playerCards);
+  let aceCounter = 0;
   for (const playerCard of playerCards) {
-    isAce = playerCard.rank === 1;
-    console.log(playerCard.rank);
-    console.log(isAce);
-    if (isAce === true) {
-      return isAce;
-      break;
+    if (playerCard.rank === 1) {
+      aceCounter += 1;
     }
   }
-  return isAce;
+  return aceCounter;
 };
-
 dealBtn.onclick = () => deal();
 hitBtn.onclick = () => hit();
 standBtn.onclick = () => stand();
-resetBtn.onclick = () => location.reload()
-
-window.addEventListener("DOMContentLoaded", () => {
-  // Execute after page load
-});
+resetBtn.onclick = () => location.reload();
+window.addEventListener("DOMContentLoaded", () => {});
