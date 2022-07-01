72c5f1ff5d02b47c5d49ee3d7d24d450883e9420
Jason <jason.blunck@gmail.com>
2022-07-01 15:19:22 -0500

    completed a working blackjack game with DOM elements. Practice with linked lists.

---
+++ week4/day5/linked-list.js
@@ -0,0 +1,25 @@
+
+//  Definition for singly-linked list.
+
+//   function listNode(val, next) {
+//       this.val = (val===undefined ? 0 : val)
+//       this.next = (next===undefined ? null : next)
+//   }
+
+//   @param {ListNode} head
+//   @param {number} n
+//   @return {ListNode}
+
+
+head = [1, 2, 3, 4, 5], n = 2;
+
+var removeNthFromEnd = function (head, n) {
+    while (head)
+    console.log(head);
+    console.log(head.value);
+    console.log(head.next);
+};
+
+removeNthFromEnd(head, n)
+
+
