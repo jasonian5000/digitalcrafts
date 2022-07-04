a010687d815e2e83b4215136277df74e9a772bed
Jason <jason.blunck@gmail.com>
2022-07-04 14:59:26 -0500

    Blackjack game, combined deal, stand, and new hand buttons into one button that looks like a deck of cards and changes function as needed. Dealer's first card is hidden until the player stands.

--- week4/day4/blackjack/main.js
+++ week4/day4/blackjack/main.js
@@ -1,21 +1,19 @@
 const dealerHand = document.getElementById("dealer-hand");
 const playerHand = document.getElementById("player-hand");
-const hitBtn = document.getElementById("hit-button");
-const dealBtn = document.getElementById("deal-button");
+const selectDecks = document.getElementById("select-decks");
 const standBtn = document.getElementById("stand-button");
-const newHandBtn = document.getElementById("new-button");
-const resetBtn = document.getElementById("reset-button")
-const selectDecks = document.getElementById("select-decks")
+const dealHitBtn = document.getElementById("deal-hit-button");
 const playerPoints = document.getElementById("player-points");
 const dealerPoints = document.getElementById("dealer-points");
+const resetBtn = document.getElementById("reset-button");
 const mainMessage = document.getElementById("main-message");
 const mainImage = document.getElementById("main-image");
-const displayWins = document.getElementById("display-wins")
-const displayLoses = document.getElementById("display-loses")
-let winCounter = Number(sessionStorage.getItem("wins"))
-let loseCounter = Number(sessionStorage.getItem("loses"))
-displayWins.innerText = `wins: ${winCounter}`
-displayLoses.innerText = `loses: ${loseCounter}`
+const displayWins = document.getElementById("display-wins");
+const displayLoses = document.getElementById("display-loses");
+let winCounter = Number(sessionStorage.getItem("wins"));
+let loseCounter = Number(sessionStorage.getItem("loses"));
+displayWins.innerText = `wins: ${winCounter}`;
+displayLoses.innerText = `loses: ${loseCounter}`;
 let aceCounter = 0;
 let aceCompare = 0;
 let dealerScore = 0;
@@ -23,9 +21,7 @@ let playerScore = 0;
 let dealerCards = [];
 let playerCards = [];
 let deck = [];
-hitBtn.disabled = true;
 standBtn.disabled = true;
-newHandBtn.disabled = true;
 const suits = ["hearts", "spades", "clubs", "diamonds"];
 const ranks = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
 const makeDeck = () => {
@@ -44,7 +40,11 @@ const makeDeck = () => {
 };
 const addCard = (hand, deck, cards, points, score) => {
   let img = new Image();
-  img.src = deck[0].imageSrc;
+  if (dealerCards.length === 0) {
+    img.src = "./images/back.png";
+  } else {
+    img.src = deck[0].imageSrc;
+  }
   hand.appendChild(img);
   score += deck[0].pointValue;
   points.innerText = score;
@@ -52,10 +52,10 @@ const addCard = (hand, deck, cards, points, score) => {
   deck.splice(0, 1);
   return score;
 };
-const repeat = (makeDeck, times=1) => {
-    makeDeck()
-    times && --times && repeat(makeDeck, times)
-}
+const repeat = (makeDeck, times = 1) => {
+  makeDeck();
+  times && --times && repeat(makeDeck, times);
+};
 const deal = () => {
   repeat(makeDeck, Number(selectDecks.value));
   dealerScore = addCard(
@@ -86,11 +86,8 @@ const deal = () => {
     playerPoints,
     playerScore
   );
-  dealBtn.disabled = true;
-  selectDecks.disabled = true
-  hitBtn.disabled = false;
+  selectDecks.disabled = true;
   standBtn.disabled = false;
-  newHandBtn.disabled = false;
   if (playerScore === 21 && dealerScore === 21) {
     tie();
   }
@@ -123,6 +120,10 @@ const hit = () => {
   }
 };
 const stand = () => {
+  let img = new Image();
+  img.src = dealerCards[0].imageSrc;
+  dealerHand.removeChild(dealerHand.firstElementChild);
+  dealerHand.replaceChild(img, dealerHand.childNodes[0]);
   while (dealerScore <= 16) {
     dealerScore = addCard(
       dealerHand,
@@ -145,23 +146,22 @@ const stand = () => {
 const win = () => {
   mainMessage.innerText = "You win!";
   mainImage.src = "./images/hansolo.png";
-  hitBtn.disabled = true;
   standBtn.disabled = true;
-  winCounter += 1
-  sessionStorage.setItem("wins", winCounter)
+  winCounter += 1;
+  sessionStorage.setItem("wins", winCounter);
+  displayWins.innerText = `wins: ${winCounter}`;
 };
 const lose = () => {
   mainMessage.innerText = "You lose!";
   mainImage.src = "./images/greedo.jpg";
-  hitBtn.disabled = true;
   standBtn.disabled = true;
-  loseCounter += 1
-  sessionStorage.setItem("loses", loseCounter)
+  loseCounter += 1;
+  sessionStorage.setItem("loses", loseCounter);
+  displayLoses.innerText = `loses: ${loseCounter}`;
 };
 const tie = () => {
   mainMessage.innerText = "What?! It's a tie!";
   mainImage.src = "./images/chewbacca.png";
-  hitBtn.disabled = true;
   standBtn.disabled = true;
 };
 const aceCheck = () => {
@@ -174,13 +174,23 @@ const aceCheck = () => {
   return aceCounter;
 };
 const resetGame = () => {
-    sessionStorage.setItem("loses", "0")
-    sessionStorage.setItem("wins", "0")
-    location.reload()
-}
-dealBtn.onclick = () => deal();
-hitBtn.onclick = () => hit();
+  sessionStorage.setItem("loses", "0");
+  sessionStorage.setItem("wins", "0");
+  location.reload();
+};
+
+const dealHitSwitch = () => {
+  if (mainMessage.innerText !== "Welcome to the Cantina") {
+    location.reload();
+  }
+  if (playerCards.length >= 2) {
+    hit();
+  }
+  if (playerCards.length < 2) {
+    deal();
+  }
+};
+dealHitBtn.onclick = () => dealHitSwitch();
 standBtn.onclick = () => stand();
-newHandBtn.onclick = () => location.reload();
-resetBtn.onclick = () => resetGame()
+resetBtn.onclick = () => resetGame();
 window.addEventListener("DOMContentLoaded", () => {});
