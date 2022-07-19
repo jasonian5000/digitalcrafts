196b3b20c23a7a60c9925dd23fdd107edde10bf8
Jason <jason.blunck@gmail.com>
2022-07-19 10:23:00 -0500

    practice with node template engine

---
+++ week7/day2/example/index.js
@@ -0,0 +1,26 @@
+import express from "express";
+import es6Renderer from "express-es6-template-engine";
+
+const app = express();
+const PORT = 3001;
+
+// middleware app.use()
+
+app.engine("html", es6Renderer);
+app.set("views", "templates");
+app.set("view engine", "html");
+
+app.get("/", (req, res) => {
+  // res.send("Hello from Express!")
+  res.render("home");
+});
+
+app.get("/joe", (req, res) => {
+  res.render("joe");
+});
+
+app.post("/home", (req, res) => {
+  res.send("Hello again from Express!");
+});
+
+app.listen(PORT, console.log(`listening on port ${PORT}`));
