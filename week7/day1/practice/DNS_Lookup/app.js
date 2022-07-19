7e02361257c67a291a49fde8bf5ada0a0f193fed
Jason <jason.blunck@gmail.com>
2022-07-19 09:32:07 -0500

    Update app.js

--- week7/day1/practice/DNS_Lookup/app.js
+++ week7/day1/practice/DNS_Lookup/app.js
@@ -4,11 +4,6 @@ let readline = createInterface({
   input: process.stdin,
   output: process.stdout,
 });
-// const dns = require("dns");
-// const readline = require("readline").createInterface({
-//   input: process.stdin,
-//   output: process.stdout,
-// });
 
 readline.question("Domain Name: ", function (url) {
   readline.close();
