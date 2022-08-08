a7044ea437adc9f71e4240a70794ed926593bd8a
Jason <jason.blunck@gmail.com>
2022-08-08 11:50:41 -0500

    typescript examples

---
+++ week10/day1/dist/demo.js
@@ -0,0 +1,10 @@
+"use strict";
+// datatypes
+// string "anything inside quotes"
+// let andrea = "andrea"
+let andrea = "andrea";
+// number
+let rahminsAge = 23;
+// boolean - true or false
+let stacysInternet = false;
+// 
